fdd4b58ea474b78e54a0f319811981a34fe4d518
Bastian Greshake Tzovaras <bgreshake@googlemail.com>
2018-08-29 14:45:40 -0700

    write dashboard

--- app/urls.py
+++ app/urls.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.urls import path, include
 
 from app import views
@@ -9,8 +8,12 @@ urlpatterns = [
     path('', views.info, name='info'),
     path('authorize/', views.authorize, name='authorize'),
     path('authenticate/', views.authenticate, name='authenticate'),
+    path('delete_user/', views.delete_user, name='delete_user'),
+    path('update_archive/',views.update_archive, name='update_archive'),
+    path('about/',views.about, name='about'),
     path('spotify_authorize/', views.spotify_authorize, name='spotify_authorize'),
     path('spotify_authenticate/', views.spotify_authenticate, name='spotify_authenticate'),
+    path('spotify_delink/', views.spotify_delink, name='spotify_delink'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('debug/', update_play_history),
     path('recommendations/', views.recommendations, name='recommendations'),
--- app/views.py
+++ app/views.py
@@ -11,12 +11,17 @@ from django.shortcuts import render, redirect, reverse
 from app.decorators import member_required
 from app.models import OpenHumansMember, SpotifyUser
 from .tasks import update_play_history
+from .helpers import get_download_url
 
 SPOTIFY_BASE_URL = 'https://api.spotify.com/v1'
 
 
 def info(request):
     return render(request, 'info.html')
+
+
+def about(request):
+    return render(request, 'about.html')
 
 
 def authorize(request):
@@ -62,7 +67,8 @@ def spotify_authorize(request):
     ]
     auth_params = {
         'client_id': os.getenv('SPOTIFY_CLIENT_ID'),
-        'redirect_uri': request.build_absolute_uri(reverse('spotify_authenticate')),
+        'redirect_uri': request.build_absolute_uri(
+            reverse('spotify_authenticate')),
         'scope': ' '.join(scopes),
         'response_type': 'code',
         'state': os.getenv('SECRET_KEY')
@@ -80,7 +86,8 @@ def spotify_authenticate(request):
                         data={
                             'grant_type': 'authorization_code',
                             'code': request.GET.get('code'),
-                            'redirect_uri': request.build_absolute_uri(reverse('spotify_authenticate')),
+                            'redirect_uri': request.build_absolute_uri(
+                                reverse('spotify_authenticate')),
                             'client_id': os.getenv('SPOTIFY_CLIENT_ID'),
                             'client_secret': os.getenv('SPOTIFY_CLIENT_SECRET')
                         }).json()
@@ -101,47 +108,87 @@ def spotify_authenticate(request):
 
 @member_required
 def dashboard(request):
-
-    if not hasattr(request.user, 'spotify_user'):
-        return redirect('spotify_authorize')
-
-    spotify_profile = requests.get(SPOTIFY_BASE_URL + '/me', headers={
-        'Authorization': 'Bearer {}'.format(request.user.spotify_user.get_access_token())
-    }).json()
-
-    available_genres = requests.get(SPOTIFY_BASE_URL + '/recommendations/available-genre-seeds', headers={
-        'Authorization': 'Bearer {}'.format(request.user.spotify_user.get_access_token())
-    }).json().get('genres')
-
-    return render(request, 'dashboard.html', context={
-        'spotify_profile': spotify_profile,
-        'available_genres': available_genres
-    })
+    if hasattr(request.user, 'spotify_user'):
+        spotify_user = request.user.spotify_user
+    else:
+        spotify_user = None
+    return render(
+        request,
+        'dashboard.html',
+        {'spotify_user': spotify_user,
+         'archive_url': get_download_url(request.user.oh_member)})
 
 
-def recommendations(request):
-
-    payload = {
-        'seed_genres': ','.join(request.POST.getlist('genres'))
-    }
-
-    for key, value in request.POST.items():
-        value = value.split(',')
-        if key.startswith('a_'):
-            payload['min_{}'.format(key[2:])] = value[0]
-            payload['max_{}'.format(key[2:])] = value[1]
-
-    recommendations = requests.get(SPOTIFY_BASE_URL + '/recommendations', headers={
-        'Authorization': 'Bearer {}'.format(request.user.spotify_user.get_access_token())
-    }, params=payload).json()
-
-    if len(recommendations['tracks']) == 0:
-        messages.add_message(request, messages.WARNING, 'No tracks found for the selected parameters')
+@member_required
+def spotify_delink(request):
+    if request.method == "POST":
+        request.user.spotify_user.delete()
         return redirect('dashboard')
 
-    return render(request, 'recommendations.html', context={
-        'recommendations': recommendations
-    })
+
+@member_required
+def delete_user(request):
+    if request.method == "POST":
+        request.user.delete()
+        return redirect('info')
+
+
+@member_required
+def update_archive(request):
+    if request.method == "POST":
+        update_play_history.delay(request.user.oh_member.oh_id)
+        return redirect('dashboard')
+
+
+@member_required
+def recommendations(request):
+    if not hasattr(request.user, 'spotify_user'):
+        return redirect('spotify_authorize')
+    if request.method == 'POST':
+        payload = {
+            'seed_genres': ','.join(request.POST.getlist('genres'))
+        }
+
+        for key, value in request.POST.items():
+            value = value.split(',')
+            if key.startswith('a_'):
+                payload['min_{}'.format(key[2:])] = value[0]
+                payload['max_{}'.format(key[2:])] = value[1]
+
+        recommendations = requests.get(
+            SPOTIFY_BASE_URL + '/recommendations',
+            headers={
+                'Authorization': 'Bearer {}'.format(
+                    request.user.spotify_user.get_access_token())
+                }, params=payload).json()
+
+        if len(recommendations['tracks']) == 0:
+            messages.add_message(
+                request,
+                messages.WARNING,
+                'No tracks found for the selected parameters')
+            return redirect('dashboard')
+
+        return render(request, 'recommendations.html', context={
+            'recommendations': recommendations
+        })
+    else:
+        spotify_profile = requests.get(SPOTIFY_BASE_URL + '/me', headers={
+            'Authorization': 'Bearer {}'.format(
+                request.user.spotify_user.get_access_token())
+        }).json()
+
+        available_genres = requests.get(
+            SPOTIFY_BASE_URL + '/recommendations/available-genre-seeds',
+            headers={
+                'Authorization': 'Bearer {}'.format(
+                    request.user.spotify_user.get_access_token())
+                    }).json().get('genres')
+
+        return render(request, 'recommendations_form.html', context={
+            'spotify_profile': spotify_profile,
+            'available_genres': available_genres
+        })
 
 
 def log_out(request):
